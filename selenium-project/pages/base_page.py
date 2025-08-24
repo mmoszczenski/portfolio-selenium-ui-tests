d9d3145c10cd2e3168190d967aa632105ca2508c
Milosz <m.mokka96@gmail.com>
2025-08-24 07:43:28 +0200

    Rewriting base_page.py for learning purposes

--- selenium-project/pages/base_page.py
+++ selenium-project/pages/base_page.py
@@ -1,40 +1,36 @@
+import selenium
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 class BasePage():
     
-    def __init__(self, driver, timeout = 10):
+    def __init__(self, driver: WebDriver, timeout = 10):
         self.driver = driver
         self.timeout = timeout
         
     def open(self, url):
-        self.driver.get(url)
+        self.driver.get(url)    
         
     def find(self, locator):
-        return WebDriverWait(self.driver, self.timeout).until(
-            EC.presence_of_element_located(locator)
-        )
-
+        return WebDriverWait(self.driver, self.timeout).until(EC.presence_of_element_located(locator))
+    
     def type(self, locator, text):
         element = self.find(locator)
         element.clear()
         element.send_keys(text)
         
     def get_text(self, locator):
-        return self.find(locator).text              
+        return self.find(locator).text
     
     def click(self, locator):
-        element = WebDriverWait(self.driver, self.timeout).until(
-            EC.element_to_be_clickable(locator)
-        )
+        element = WebDriverWait(self.driver, self.timeout).until(EC.element_to_be_clickable(locator))
         element.click()
         
     def is_visible(self, locator):
         try:
-            WebDriverWait(self.driver, self.timeout).until(
-                EC.visibility_of_element_located(locator)
-            )
-            return True
-        except:
-            return False    
+            element = WebDriverWait(self.driver, self.timeout).until(EC.visibility_of_element_located(locator))
+            return element
+        except TimeoutError:
+            return None
