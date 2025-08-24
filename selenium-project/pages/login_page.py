224d5390d896ec2ea3d7691ccecb238021256316
Milosz <m.mokka96@gmail.com>
2025-08-24 09:33:33 +0200

    adding method to login_page.py

--- selenium-project/pages/login_page.py
+++ selenium-project/pages/login_page.py
@@ -1,6 +1,10 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
 
 class LoginPage(BasePage):
     
-    _HEADER = (By.ID, "")
+    _HEADER = (By.XPATH, "//h2[text()='New User Signup!']")
+    
+    def is_on_login_page(self):
+        return self.is_visible(self._HEADER)
--- selenium-project/tests/test_register.py
+++ selenium-project/tests/test_register.py
@@ -1,4 +1,5 @@
 from pages.home_page import HomePage
+from pages.login_page import LoginPage
 import pytest
 
 class TestRegister():
