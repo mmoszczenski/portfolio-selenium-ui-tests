6282fa8cc2b1da571b0fad6e998be8db5eae2c3f
Milosz <m.mokka96@gmail.com>
2025-08-24 09:26:42 +0200

    setup and execution of first test

--- selenium-project/tests/test_example.py
+++
@@ -1,4 +0,0 @@
-
-def test_google(driver):
-    driver.get("https://www.google.com")
-    assert "Google" in driver.title
--- selenium-project/tests/test_register.py
+++ selenium-project/tests/test_register.py
@@ -4,9 +4,11 @@ import pytest
 class TestRegister():
     
     def test_register_valid_user(self, pages):
+        
         home_page: HomePage = pages["home"]
         
-        home_page
+        home_page.open(home_page.URL)
+        home_page.is_on_homepage
     
         
     
