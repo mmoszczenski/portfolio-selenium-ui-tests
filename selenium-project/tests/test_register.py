3c8c093827bab5a0fd50ed59224b56a633dfbb88
Milosz <m.mokka96@gmail.com>
2025-08-24 09:35:45 +0200

    Updating registration test

--- selenium-project/tests/test_register.py
+++ selenium-project/tests/test_register.py
@@ -7,9 +7,12 @@ class TestRegister():
     def test_register_valid_user(self, pages):
         
         home_page: HomePage = pages["home"]
+        login_page: LoginPage = pages["login"]
         
         home_page.open(home_page.URL)
         home_page.is_on_homepage
+        
+        login_page.is_on_login_page
     
         
     
