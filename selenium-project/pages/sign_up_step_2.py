07e70fe9fe2a507d8c227a7e9b299b0013178d18
Milosz <m.mokka96@gmail.com>
2025-08-26 18:55:22 +0200

    adding radio selectors and select_title method

--- selenium-project/pages/sign_up_step_2.py
+++ selenium-project/pages/sign_up_step_2.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.by import By
 class SignUpStep2(BasePage):
     
     _PAGE_HEADER = (By.XPATH, "//div[contains(@class,'login-form')]//h2[b[text()='Enter Account Information']]")
+    _title = {
+        "Mr": (By.ID, "id_gender1"),
+        "Mrs": (By.ID, "id_gender2")
+    }
     
     def is_on_sign_up_step2_page(self):
         return self.is_visible(self._PAGE_HEADER)
+    
+    def select_title(self, title):
+        self.click(self._title[title])
