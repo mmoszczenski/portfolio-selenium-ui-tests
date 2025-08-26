1a390cd95dbb6a4e77ebfd05201d360ce61651c3
Milosz <m.mokka96@gmail.com>
2025-08-26 18:02:13 +0200

    adding test_data content

--- selenium-project/data/test_data.py
+++ selenium-project/data/test_data.py
@@ -1,4 +1,15 @@
 
 SIGNUP_DATA = {
-"valid_user": {"username": "user1", "email": "example@email.com", "password": "passwordtest"}
+"valid_user": {
+    "username": "test_user", 
+     "email": "example@email.com",
+     "password": "passwordtest"
+     },
+
+"invalid_email_user": {
+    "username": "test_user",
+    "email": "invalidemail.com",
+    "password": "passwordtest"
+},
+
 }
