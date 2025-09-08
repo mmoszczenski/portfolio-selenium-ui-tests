18215ec955d0cfcc8995d44dd9d5d5b91a868914
Milosz <m.mokka96@gmail.com>
2025-09-08 19:47:26 +0200

    part 1 of api helper for create user

--- selenium-project/tests/test_register.py
+++ selenium-project/tests/test_register.py
@@ -118,12 +118,33 @@ class TestRegisterNegative():
         
         assert sign_up_step2_page.is_password_valid() is False
         assert sign_up_step2_page.is_password_type_valueMissing() is True
+         
+    def test_register_with_empty_fields(self, pages):
         
-        time.sleep(2)
-       
-    #def test_register_with_empty_fields():
-       
-    #def test_register_with_special_characters():
+        user = SIGNUP_DATA["valid_user"]
+        home_page: HomePage = pages["home"]
+        login_page: LoginPage = pages["login"]
+        sign_up_step2_page: SignUpStep2 = pages["sign_up_step2"]
+        
+        home_page.open(home_page.URL)
+        home_page.accept_cookies()
+        home_page.go_to_login_page()
+        
+        login_page.sign_up(user["username"], user["email"])    
+        
+        sign_up_step2_page.fill_account_information_form(
+            country = "",
+            first_name = "",
+            last_name = "",
+            address = "",
+            state = "",
+            city = "",
+            zipcode = "",
+            mobile_number = ""            
+        )                 
+    
+        assert sign_up_step2_page.is_password_valid() is False
+        assert sign_up_step2_page.is_password_type_valueMissing() is True
     
     #def test_register_with_taken_email():
       
--- selenium-project/utils/helpers.py
+++ selenium-project/utils/helpers.py
@@ -2,3 +2,4 @@ import uuid
 
 def generate_random_email(domain="example.com"):
     return f"user_{uuid.uuid4().hex[:8]}@{domain}"
+
