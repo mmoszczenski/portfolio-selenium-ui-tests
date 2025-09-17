382e8e0974b68411a5d13c56a88b4e1ec0c19060
Milosz_Work <m.mokka96@gmail.com>
2025-09-17 11:32:13 +0200

    added test_login file

---
+++ selenium-project/tests/test_login.py
@@ -0,0 +1,42 @@
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.main_page_logged_in import MainPageLoggedIn
+from data.test_data import SIGNUP_DATA
+
+
+class TestLoginPositive():
+
+#Test Case 1 - User provides valid login and password and then user is redirected to the proper page
+
+    def test_login_with_valid_credentials(self, pages):
+        pass
+
+
+
+
+
+
+
+
+
+#Test Case 2 - User logins in and then logs out and no longer has acess to the logged in resources
+
+
+
+
+
+
+
+
+class TestLoginPositive():
+    pass
+
+
+#Test Case 1 - User provides valid login and invalid password. There is proper error message
+
+
+#Test Case 3 - User provides non-existing login and tries to submit the form. There is porper error message
+
+#Test Case 4 - User doesn't provide any data what so ever and submits the form
+
+#Test Case 5 - 
