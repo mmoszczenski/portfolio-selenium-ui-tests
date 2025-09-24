454f8207d28bc36fc5515621ab31967fcdae0924
Milosz <m.mokka96@gmail.com>
2025-09-24 19:55:31 +0200

    adding ContactForm locators

--- selenium-project/pages/contact_form_page.py
+++ selenium-project/pages/contact_form_page.py
@@ -2,4 +2,12 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class ContactForm(BasePage):
-    pass
+
+    _NAME_INPUT_FIELD = (By.CSS_SELECTOR, "input['data-qa=name']")
+    _EMAIL_INPUT_FIELD = (By.CSS_SELECTOR, "input['data-qa=email']")
+    _SUBJECT_INPUT_FIELD = (By.CSS_SELECTOR, "input['data-qa=subject']")
+    _MESSAGE_INPUT_FIELD = (By.CSS_SELECTOR, "input['data-qa=message']")
+    _SUBMIT_BUTTON = (By.CSS_SELECTOR, "input['data-qa=submit-button']")
+    _UPLOAD_BUTTON = (By.CSS_SELECTOR, "input['name=upload_file']")
+    
+    
