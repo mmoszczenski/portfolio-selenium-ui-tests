93a6789d6d76e9cc20794726c33d56b653f58dee
Milosz <m.mokka96@gmail.com>
2025-09-24 19:10:28 +0200

    added ContactForm test

--- selenium-project/pages/home_page.py
+++ selenium-project/pages/home_page.py
@@ -6,9 +6,10 @@ class HomePage(BasePage):
     URL = "https://automationexercise.com/"
     
     _ACCEPT_COOKIES_BUTTON = (By.CSS_SELECTOR, "button[aria-label='Zgadzam się']")
+    _LOGOUT_BUTTON = (By.CSS_SELECTOR, "a[href='/logout']")
+    
     _LOGIN_BUTTON = (By.CSS_SELECTOR, "a[href='/login']" )
-
-    _LOGOUT_BUTTON = (By.CSS_SELECTOR, " a[href='/logout']")
+    _CONTACT_FORM_BUTTON = (By.CSS_SELECTOR, "a[href='/contact_us']")
     
             
     def is_on_homepage(self) -> bool:
@@ -16,6 +17,9 @@ class HomePage(BasePage):
        
     def go_to_login_page(self):
         self.click(self._LOGIN_BUTTON)
+        
+    def go_to_contact_form_page(self):
+        self.click(self._CONTACT_FORM_BUTTON)
         
     def accept_cookies(self):
         self.click(self._ACCEPT_COOKIES_BUTTON)
---
+++ selenium-project/tests/test_contact_form.py
@@ -0,0 +1,22 @@
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.sign_up_step_2 import SignUpStep2
+from pages.account_created_page import AccountCreatedPage
+
+
+class TestContactFormPositive():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+class TestContactFormNegative():
+    pass
