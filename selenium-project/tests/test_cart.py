aa9d25cfef483360dca9976294b6f81a84d4639d
Milosz <m.mokka96@gmail.com>
2025-10-04 19:04:34 +0200

    added removal test

--- selenium-project/tests/test_cart.py
+++ selenium-project/tests/test_cart.py
@@ -30,6 +30,7 @@ class TestCartPositive:
         actual_total = cart_page.product_total(product_id)
         
         assert cart_page.is_cart_page_displayed(), "Cart page not displayed"
+        assert cart_page.products_count == 1, "Product count is incorrect"
         assert expected_name == actual_name, "Product name differs"
         assert expected_total == actual_total, "Total value is incorrect"
         
@@ -60,8 +61,7 @@ class TestCartPositive:
 
         assert cart_page.is_cart_page_displayed(), "Cart page not displayed"
         assert expected_total == actual_total, "Expected total is different than actual total"     
-        time.sleep(3)
-        assert cart_page.products_count() == 2, "Product quantity is invalid"   
+        assert cart_page.products_count() == 2, "Product count is incorrect"   
         
 
     def test_remove_item_from_cart(self, pages):
@@ -87,10 +87,12 @@ class TestCartPositive:
         product_page.click_view_cart_btn()
         
         cart_page.remove_product(product_ids[1])
+        cart_page.reload_page()
         
         actual_total = cart_page.product_total(product_ids[0])
         
         assert expected_total == actual_total
+        assert cart_page.products_count() == 1, "Product count is incorrect"
     
     
     def test_add_to_cart_from_various_pages(self):
