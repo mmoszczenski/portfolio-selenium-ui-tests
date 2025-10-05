34aaafa9bbab8d60ea68cc5890ad2c6c920b99c6
Milosz <m.mokka96@gmail.com>
2025-10-05 08:00:42 +0200

    refactored local variable naming

--- selenium-project/pages/product_details_page.py
+++ selenium-project/pages/product_details_page.py
@@ -6,11 +6,10 @@ class ProductDetailsPage(BasePage):
     _QUANTITY_FIELD = (By.ID, "quantity")
     _ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn.cart")
     
-    
     def change_quantity(self, quantity: int):
-        field = self.find(self._QUANTITY_FIELD)
-        field.clear()
-        field.send_keys(quantity)
+        quantity_input= self.find(self._QUANTITY_FIELD)
+        quantity_input.clear()
+        quantity_input.send_keys(quantity)
         
     def add_product_to_cart(self):
         self.click(self._ADD_TO_CART_BUTTON)
