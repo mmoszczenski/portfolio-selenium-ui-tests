2330dbbfaf9b5da790fe5bc95acdf0f934661dcc
Milosz_Work <m.mokka96@gmail.com>
2025-10-08 11:47:50 +0200

    added screenshot option for failed tests

--- selenium-project/tests/conftest.py
+++ selenium-project/tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+import pytest_html
+import os
+from datetime import datetime
 from utils.driver_factory import create_driver
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
@@ -37,3 +40,26 @@ def pages(driver):
         "payment_page": PaymentPage(driver),
         "order_placed_page": OrderPlacedPage(driver)
     }
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    # Jeśli test się nie powiódł
+    if rep.failed:
+        driver = item.funcargs.get("driver")  # fixture driver
+        if driver:
+            screenshots_dir = "screenshots"
+            os.makedirs(screenshots_dir, exist_ok=True)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            file_path = os.path.join(screenshots_dir, f"{item.name}_{timestamp}.png")
+            driver.save_screenshot(file_path)
+            print(f"\nScreenshot saved: {file_path}")
+
+
+            if hasattr(rep, "extra"):
+                rep.extra.append(pytest_html.extras.image(file_path))
+            else:
+                rep.extra = [pytest_html.extras.image(file_path)]
