98c9444e8bd4036b8e203a428fabf9057c8f8707
Milosz_Work <m.mokka96@gmail.com>
2025-10-08 11:07:54 +0200

    changed driver factory for CI integration

--- selenium-project/utils/driver_factory.py
+++ selenium-project/utils/driver_factory.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import tempfile
 
 def create_driver(headless: bool = False):
     options = Options()
     options.add_argument("--start-maximized")
-    # options.add_argument("--incognito")
     if headless:
         options.add_argument("--headless=new")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-gpu")
-    service = ChromeService()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+    user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    # poprawny path do chromedriver w obrazie Playwright
+    service = Service("/usr/bin/chromedriver")
+
     driver = webdriver.Chrome(service=service, options=options)
     return driver
