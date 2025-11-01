fbe5dc89bf7efd806c746d7330fbde171ac0c7d5
Milosz <m.mokka96@gmail.com>
2025-11-01 16:05:13 +0100

    finished search test

--- selenium-project/pages/products_page.py
+++ selenium-project/pages/products_page.py
@@ -10,8 +10,10 @@ class ProductsPage(BasePage):
     _VIEW_CART_BUTTON = (By.CSS_SELECTOR, "a[href='/view_cart']")
     _MODAL = (By.CLASS_NAME, "modal-content")
     
-    _SEARCH_INPUT = (By.ID, "search-input")
+    _SEARCH_INPUT = (By.ID, "search_product")
     _SUBMIT_SEARCH_BUTTON = (By.ID, "submit_search")
+    
+    _PRODUCT_NAMES = (By.CSS_SELECTOR, ".single-products")
     
     def add_to_cart_by_id(self, product_id: int):
         selector = f"{self._ADD_TO_CART_BUTTON}[data-product-id='{product_id}']"
@@ -37,3 +39,12 @@ class ProductsPage(BasePage):
         self.type(self._SEARCH_INPUT, text)
         self.click(self._SUBMIT_SEARCH_BUTTON)
     
+    def get_products_name(self):
+        products = self.find_all(self._PRODUCT_NAMES)
+        return [product.text.strip() for product in products]
+    
+    def contains_product_with_text(self, text):
+        names = self.get_products_name()
+        return any(text.lower() in name.lower() for name in names)
+        
+        
--- selenium-project/tests/test_product_search.py
+++ selenium-project/tests/test_product_search.py
@@ -8,9 +8,14 @@ class TestProductSearch:
         
         home_page: HomePage = pages["home"]
         products_page: ProductsPage = pages["products_page"] 
-    
+        
+        search_phrase = "Blue top"
     
         home_page.open_homepage()
         home_page.go_to_products_page()
         
-        products_page.use_search_input("Blue top")
+        products_page.use_search(search_phrase)
+        home_page.remove_ads_banner_if_visible()
+        
+        assert products_page.contains_product_with_text(search_phrase), "No result with given search phrase were found"
+        
